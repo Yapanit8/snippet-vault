48e072f3561a81695d2865e5e891390bb668dd62
Yapanit <karlsanjosedev08@gmail.com>
2026-04-25 21:08:23 +0800

    feat: add app header

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'blue' // or whatever color you want
+      primary: 'green'
     }
   }
 })
---
+++ app/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <header class="h-16 flex justify-center border-b bg-white">
+      <div class="flex justify-between items-center px-4 w-full max-w-7xl">
+        <div class="flex gap-2 items-center">
+          <div class="bg-indigo-600 w-10 h-10 rounded-xl flex items-center justify-center shadow-lg">
+            <UIcon name="i-heroicons-code-bracket" class="w-6 h-6 text-white" />
+          </div>
+          <h1 class="text-xl font-bold text-slate-900 tracking-tight">SnippetVault</h1>
+        </div>
+        
+        <div class='flex items-center gap-3'>
+          <UInput class='w-50 md:w-full'  icon="i-lucide-search" placeholder='Search snippets...' input-class="text-black"/>
+          <UButton icon="i-lucide-plus" class='text-white'>
+            <span class='hidden whitespace-nowrap md:block'>New Snippet</span>
+          </UButton>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
