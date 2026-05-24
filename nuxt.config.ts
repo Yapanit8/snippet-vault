ecef28c996548697937c64a7941dec54c33bc994
Yapanit <karlsanjosedev08@gmail.com>
2026-05-24 23:08:56 +0800

    ci: add GitHub Actions CI workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
   css: ['~/assets/css/main.css']
 })
