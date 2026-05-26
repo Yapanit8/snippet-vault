b2668dc9524056b3b29ce747e218af17c8ab0ee2
Yapanit <karlsanjosedev08@gmail.com>
2026-05-27 00:27:09 +0800

    feat:create add new snippet layout and dialog

--- app/components/SnippetCards.vue
+++ app/components/SnippetCards.vue
@@ -1,13 +1,9 @@
 <template>
   <div class='flex flex-wrap gap-8'>
-    <UCard 
-      variant="solid" 
-      class="w-full group lg:w-md hover:shadow-md transition-shadow duration-250" 
-      :ui="{
-        root: 'border-2 border-gray-200 rounded-xl',
-        body: 'sm:py-2 px-6'
-      }"
-    >
+    <UCard variant="solid" class="w-full group lg:w-md hover:shadow-md transition-shadow duration-250" :ui="{
+      root: 'border-2 border-gray-200 rounded-xl',
+      body: 'sm:py-2 px-6'
+    }">
       <template #header>
         <div class='flex justify-between'>
           <div>
@@ -19,14 +15,8 @@
             </div>
           </div>
           <div class='opacity-0 group-hover:opacity-100 transition-opacity'>
-            <UButton
-              icon="i-heroicons-document-duplicate-solid"
-              variant="ghost"
-            />
-            <UButton
-              icon='i-mdi-trash'
-              variant='ghost'
-            />
+            <UButton icon="i-heroicons-document-duplicate-solid" variant="ghost" />
+            <UButton icon='i-mdi-trash' variant='ghost' />
           </div>
         </div>
       </template>
@@ -42,26 +32,28 @@
     </UCard>
 
     <!-- ADD SNIPPET CARD -->
-    <UCard
-      class="w-full group lg:w-md flex justify-center items-center"
-      :ui="{ 
-        root: 'border-3 border-dashed ring-0 hover:border-primary transition-all duration-400' 
-      }"
-    >
+    <UCard class="w-full group lg:w-md flex justify-center items-center" :ui="{
+      root: 'border-3 border-dashed ring-0 hover:border-primary transition-all duration-400'
+    }" @click='isDialogOpen = true'>
       <div class='flex flex-col gap-5 justify-center items-center'>
-        <UButton
-          variant='soft'
+        <UButton variant='soft'
           class='rounded-full w-[3rem] h-[4rem] flex justify-center text-gray-400 group-hover:text-primary transition-all duration-400'
-          size='xl'
-          icon='i-lucide-plus'
-        />
+          size='xl' icon='i-lucide-plus'/>
         <span class='text-gray-400 font-bold'>Add another snippet</span>
       </div>
     </UCard>
+
+    <!-- ADD NEW SNIPPET DIALOG -->
+    <UModal v-model:open='isDialogOpen' :dismissible='false'>
+      <template #content>
+        <DialogsAddNewSnippet/>
+      </template>
+    </UModal>
   </div>
 </template>
 
 <script setup lang="ts">
+let isDialogOpen = ref(false)
 </script>
 
 <style scoped></style>
---
+++ app/components/dialogs/AddNewSnippet.vue
@@ -0,0 +1,46 @@
+<template>
+  <UCard 
+    title="Add New Snippet" 
+    class="w-full bg-gray-100"
+    :ui="{
+      header:'border-b-0',
+      body:'border-y border-gray-400 bg-white'
+    }"
+    >
+    <UForm>
+      <UFormField label='Snippet Title' :ui="{ label: 'text-black' }">
+        <UInput class='w-full'></UInput>
+      </UFormField>
+      <UFormField label='Stack Used' :ui="{ label: 'text-black' }">
+        <UInputMenu :items="['Typescript', 'SQL']" class='w-full' :ui="{ item: 'text-black' }" multiple />
+      </UFormField>
+      <UFormField label='Code Snippet' :ui="{ label: 'text-black' }">
+        <UTextarea placeholder='// Paste your code here...' class='w-full' :ui="{base:'bg-slate-900 rounded-lg p-4 mb-4 text-xs overflow-x-auto border text-white'}"></UTextarea>
+      </UFormField>
+      <UFormField label='Hashtags' :ui="{ label: 'text-black' }">
+        <UInputMenu :items='[]' mode="autocomplete" :trailing-icon="false" :content="{ hideWhenEmpty: true }" class='w-full' :ui="{ item: 'text-black' }" multiple />
+      </UFormField>
+    </UForm>
+
+    <template #footer>
+      <div class='flex justify-end gap-2'>
+        <UButton
+          label='Cancel'
+          variant='outline'
+        />
+        <UButton
+          label='Save'
+          class='text-white'
+        />
+      </div>
+    </template>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
